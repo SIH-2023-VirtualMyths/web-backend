80d7353e9a9cbf715b64adaf8e3a369cc86aa576
Tanvi Srivastava <tanvi12pr@gmail.com>
2023-12-18 01:15:58 +0530

    app home

--- index.js
+++ index.js
@@ -75,3 +75,5 @@ app.post('/checkDomain', isMalicious, resolveIP, async(req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+module.exports = {app,udpServer}
